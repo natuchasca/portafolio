dce17dd40b4af1ddd173586553373d390bbf1460
natuchasca <natuxalee@gmail.com>
2016-08-03 12:14:34 -0400

    responsive y detalles de nav corregidos

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').on('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
